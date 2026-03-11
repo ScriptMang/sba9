93f9b7f0392af5be1f37c8bcec05cae1f787a4c6
scriptMang <aperalta686@gmail.com>
2026-03-11 12:45:14 -0400

    Add task-status-handler to task-list as prop for on-status-change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import type {TaskStatus} from './types/index.ts' 
 import type {Task} from './types/index.ts'
 import TaskList from './components/TaskList/TaskList'
 
@@ -14,9 +15,25 @@ function App() {
         dueDate: "12/31/2023"
   }])
 
+  const taskStatusHandler = (taskId: string, newStatus: TaskStatus) => {
+  const targetTaskId = Number(taskId)
+  console.log("Inside the task handler the str_id to num_id is: ", targetTaskId);
+  if (tasks !== null) {
+    setTasks(prevTasks =>
+      (prevTasks as Task[]).map(task =>
+        Number(task.id) === targetTaskId ? {...task, status: newStatus} : task
+      )
+    );
+    console.log(`whats the value of targetTaskId: ${targetTaskId}`);
+    console.log(`changed task status style to ${tasks[targetTaskId-1].status}`);
+  } else {
+    console.log("couldn't change the task's status style, its value is null.")
+  }
+}
+
   return (
     <>
-    <TaskList tasks={tasks} onStatusChange={()=>{}} onDelete={()=>{}}/>
+    <TaskList tasks={tasks} onStatusChange={taskStatusHandler} onDelete={()=>{}}/>
     </>
   )
 }
